aca95ad6c46fdb2f68dd4494a3f507a7e5938d86
I056949 <anton.leschuk@yandex.ru>
2018-05-08 01:27:46 +0300

    smoke selenuium tests

--- selenium-python/smoke-selenium-tests.py
+++ selenium-python/smoke-selenium-tests.py
@@ -1,4 +1,6 @@
 import unittest
+import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,21 +12,25 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 
+
+
 class SeleniumPythonDemoClass(unittest.TestCase):
 
     def setUp(self):
         #self.driver = webdriver.Firefox()
         #self.driver = webdriver.Chrome()
 
+        #self.driver = webdriver.Remote(
+        #    command_executor='http://127.0.0.1:4444/wd/hub',
+        #    desired_capabilities=DesiredCapabilities.CHROME)
+
         self.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4444/wd/hub',
-            desired_capabilities=DesiredCapabilities.CHROME)
+            desired_capabilities=DesiredCapabilities.FIREFOX)
 
-#        self.driver = webdriver.Remote(
-#            command_executor='http://127.0.0.1:4444/wd/hub',
-#            desired_capabilities=DesiredCapabilities.FIREFOX)
-
-
+    @allure.story('Your Story here')
+    @allure.feature('Your Feature here')
+    @pytest.allure.step("Launch site")
     def test_search_SAP_in_Google(self):
         driver = self.driver
         driver.get("http://www.python.org")
@@ -34,12 +40,14 @@ class SeleniumPythonDemoClass(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         assert "No results found." not in driver.page_source
 
+    @pytest.allure.step("Launch site")
     def test_simple_availibility(self):
         print("test2")
         self.driver.get("https://www.sap.com/index.html")
         assert "SAP" in self.driver.title
         assert "No results found." not in self.driver.page_source
 
+    @pytest.allure.step("Launch site")
     def test_best_practices_search(self):
         self.driver.get("https://www.sap.com/index.html")
         #element =  self.driver.find_element(By.XPATH, '//*[@id="header"]/div[3]/div/div/div/nav/ul/li[4]/a')
@@ -48,6 +56,9 @@ class SeleniumPythonDemoClass(unittest.TestCase):
 
 
     def tearDown(self):
-        #self.driver.close()
-        self.driver.Quit();
+        self.driver.close()
+        #self.driver.Quit();
         #self.driver.Dispose()
+
+if __name__ == '__main__':
+    unittest.main()
