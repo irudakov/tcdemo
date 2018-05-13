6b054523be6bc0e454b6b5a9b26ce06bd63adbdb
I056949 <anton.leschuk@yandex.ru>
2018-05-14 01:04:05 +0300

    new tests

--- selenium-python/test_smoke_selenium_version_2.py
+++ selenium-python/test_smoke_selenium_version_2.py
@@ -1,7 +1,7 @@
 import unittest
 import pytest
 import allure
-from allure.constants import AttachmentType
+#from allure.constants import AttachmentType
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -21,13 +21,13 @@ class SeleniumPythonDemoClass(unittest.TestCase):
         #self.driver = webdriver.Firefox()
         #self.driver = webdriver.Chrome()
 
-        #self.driver = webdriver.Remote(
-        #    command_executor='http://127.0.0.1:4444/wd/hub',
-        #    desired_capabilities=DesiredCapabilities.CHROME)
-
         self.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4444/wd/hub',
-            desired_capabilities=DesiredCapabilities.FIREFOX)
+            desired_capabilities=DesiredCapabilities.CHROME)
+
+        #self.driver = webdriver.Remote(
+        #    command_executor='http://127.0.0.1:4444/wd/hub',
+        #    desired_capabilities=DesiredCapabilities.FIREFOX)
 
     @allure.story('Your Story here')
     @allure.feature('Your Feature here')
@@ -37,33 +37,26 @@ class SeleniumPythonDemoClass(unittest.TestCase):
     def test_search_SAP_in_Google(self):
 
         driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
-        allure.attach('screenshot', self.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        driver.get("http://imsabap:1080/abapwiki ")
+        self.assertIn("Welcome", driver.title)
 
 
     @pytest.allure.step("Launch site")
     def test_simple_availibility(self):
-        print("test2")
-        self.driver.get("https://www.sap.com/index.html")
-        assert "SAP" in self.driver.title
-        assert "No results found." not in self.driver.page_source
-        allure.attach('screenshot', self.driver.get_screenshot_as_png(), type=AttachmentType.PNG)
+        driver = self.driver
+        driver.get("http://imsabap:1080/abapwiki ")
+        self.assertIn("Welcome", driver.title)
 
     @pytest.allure.step("Launch site")
     def test_best_practices_search(self):
-        self.driver.get("https://www.sap.com/index.html")
-        #element =  self.driver.find_element(By.XPATH, '//*[@id="header"]/div[3]/div/div/div/nav/ul/li[4]/a')
-        #aelement.click()
+        driver = self.driver
+        driver.get("http://imsabap:1080/abapwiki ")
+        self.assertIn("Welcome", driver.title)
 
 
     def tearDown(self):
         #self.driver.close()
-        self.driver.Quit();
+        self.driver.quit()
         #self.driver.Dispose()
 
 
